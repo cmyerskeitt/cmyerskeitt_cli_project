77fc42c5554511f40deec0384292e6defadeac4e
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-14 19:19:33 +0000

    initial setup

---
+++ lib/cmyerskeitt_cli_project.rb
@@ -0,0 +1,6 @@
+require "cmyerskeitt_cli_project/version"
+
+module CmyerskeittCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cmyerskeitt_cli_project/version.rb
@@ -0,0 +1,3 @@
+module CmyerskeittCliProject
+  VERSION = "0.1.0"
+end
